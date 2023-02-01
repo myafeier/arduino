b23619296e65c8c36843a22f2bf3da96db56e9b7
shon <shon@u1200.com>
2023-02-01 23:39:31 +0800

    ci

--- test/main.go
+++ test/main.go
@@ -21,9 +21,39 @@ func main() {
 	switch *cmd {
 	case "move":
 		doMove()
+	case "zoom":
+		doZoom()
+	case "pop":
+		doPop()
+	case "push":
+		doPush()
+	case "off":
+		doLaseroff()
 	}
 }
+func doLaseroff() {
+	hjscanner.DefaultScaner.RunInstruction(hjscanner.InstructionOfCloseLaser, "green")
+}
+func doPop() {
+	hjscanner.DefaultScaner.RunInstruction(hjscanner.InstructionOfMoveOut)
+}
+func doPush() {
 
+	hjscanner.DefaultScaner.RunInstruction(hjscanner.InstructionOfMoveIn)
+}
+func doZoom() {
+	for {
+		var x float32
+		fmt.Println("input zoom in/out value:")
+		fmt.Scanf("%f", &x)
+		res, err := hjscanner.DefaultScaner.RunInstruction(hjscanner.InstructionOfMoveZ, x)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println("resp: ", res)
+	}
+}
 func doMove() {
 
 	for {
